c0be9563f090df905c14e2dcc1370a6e7ab2d62c
David Pascoal <david.pascoal@inw-tech.com>
2016-11-09 18:33:35 +0000

    Reworked calculator logic, buggy behavior for negative numbers, double not yet implemented

--- src/app/arithmetic.service.ts
+++ src/app/arithmetic.service.ts
@@ -4,12 +4,18 @@ import { Injectable } from '@angular/core';
 export class ArithmeticService {
 
   result: number = 0;
+  /*
   buffer: string = '';
   cleanFlag = 0;
   finalResult: number = null;
-  signalBuffer: string = '';
+  
+  firstOperation: boolean = true;*/
 
-  firstOperation: boolean = true;
+  operationString: string = '';
+  currentOperation: string = '';
+  lastOperation: string = '';
+  signalBuffer: string = '';
+  finalResult: string = '';
 
   constructor() { }
 
@@ -26,41 +32,87 @@ export class ArithmeticService {
     }
   }
 
+  breakActionsIntoArray(input: string): Array<any> {
+    let currentBuffer = '';
+    let arrayOfOperations: Array<any> = new Array<any>();
+
+    for (let i = 0; i < input.length; i++) {
+      if (input.charAt(i) !== 'x' && input.charAt(i) !== '-' && input.charAt(i) !== '/' && input.charAt(i) !== '+') {
+        currentBuffer = currentBuffer.concat(input.charAt(i));
+      } else {
+        arrayOfOperations.push(currentBuffer);
+        arrayOfOperations.push(input.charAt(i));
+
+        currentBuffer = '';
+      }
+    }
+
+    arrayOfOperations.push(currentBuffer);
+
+    console.log('actions:', arrayOfOperations);
+
+    return arrayOfOperations;
+  }
+
+  executeOperations(operationString: string): number {
+    let result = 0;
+    let arrayOfOperations: Array<any> = this.breakActionsIntoArray(operationString);
+
+    result = parseInt(arrayOfOperations[0], 10);
+
+    for (let i = 0; i < arrayOfOperations.length - 1; i = i + 2) {
+      result = this.applyOperation(arrayOfOperations[i + 1], result, parseInt(arrayOfOperations[i + 2], 10));
+    }
+
+    return result;
+  }
+
+  clearVars(): void {
+    this.currentOperation = '';
+    this.operationString = '';
+    this.lastOperation = '';
+  }
+
   inputEvent(input: any) {
 
+    this.finalResult = '';
+
     if (input === 'C') {
-      this.buffer = '';
-      this.result = 0;
-      this.cleanFlag = 0;
-      this.finalResult = null;
-      this.firstOperation = true;
+      this.clearVars();
 
     } else if (input === '=') {
-      this.buffer = '';
-      this.result = this.applyOperation(this.signalBuffer, this.finalResult, this.result);
-      this.cleanFlag = 1;
-      this.finalResult = null;
-      this.firstOperation = true;
-
-    } else if (input === '+' || input === '-' || input === 'x' || input === '/') {
-      this.buffer = this.buffer.concat(this.result.toString()).concat(input);
-      this.cleanFlag = 1;
-      if (!this.firstOperation) {
-        this.finalResult = this.applyOperation(input, this.finalResult, this.result);
+      if (this.currentOperation && this.signalBuffer.length === 0) {
+        this.operationString = this.operationString.concat(this.currentOperation);
+        console.log(this.operationString);
+        this.finalResult = this.executeOperations(this.operationString).toString();
+        this.clearVars();
       }
+    } else if (input === '+' || input === '-' || input === 'x' || input === '/') {
+      if (this.signalBuffer.length === 0) {
+        this.lastOperation = this.currentOperation;
+      }
+      this.currentOperation = input;
       this.signalBuffer = input;
-      this.firstOperation = false;
+
     } else if (input === '±') {
-      this.result = this.result * (-1);
+      // If the last action wasn't an operation
+      if (this.signalBuffer.length === 0) {
+        this.currentOperation = (parseInt(this.currentOperation, 10) * (-1)).toString();
+      }
+
     } else if (input === '.') {
+      // TODO
 
     } else {
-      if (this.cleanFlag === 1) {
-        this.result = input;
+      if (this.signalBuffer.length > 0) {
+        this.operationString = this.operationString.concat(this.lastOperation).concat(this.signalBuffer);
+        this.currentOperation = input;
+        this.signalBuffer = '';
+        this.lastOperation = '';
       } else {
-        this.result = parseInt(this.result.toString().concat(input), 10);
-        this.finalResult = this.finalResult === null ? this.result : this.finalResult;
+        this.currentOperation = this.currentOperation.concat(input).toString();
       }
+      //this.operationString = this.operationString.concat(input);
     }
   }
 
